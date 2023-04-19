cddbfe6fd4b7358060eb782dc74d9007db045128
unknown <pneel361@gmail.com>
2023-04-19 10:18:45 +0530

    added 2 spline scene

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/v-rv-h08s3E8fUD4/scene.splinecode" />
+    <>
+    <Spline style={{height:"300px"}}  scene="https://prod.spline.design/v-rv-h08s3E8fUD4/scene.splinecode" />
+
+    <Spline style={{height:"300px"}}  scene="https://prod.spline.design/jEctKO20RCFNEH5a/scene.splinecode" />
+
+    </>
   );
 }
 
